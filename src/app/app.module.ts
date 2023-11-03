08d386b74ffbf5c64edc9f93afc0537daeeccc34
heldinho <hfpsis25@gmail.com>
2023-11-03 14:15:27 -0300

    fix
    Signed-off-by: heldinho <hfpsis25@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ButtonComponent } from './shared/components/button/button.component';
 import { StoreModule } from '@ngrx/store';
-// import { cartReducer } from './shared/store/reducers/cart.reducer';
+import { cartReducer } from './shared/store/reducers/cart.reducer';
 import { tutorialReducer } from './shared/store/reducers/tutorial.reducer';
 import { ReadComponent } from './shared/components/read/read.component';
 import { CreateComponent } from './shared/components/create/create.component';
--- src/app/shared/components/create/create.component.ts
+++ src/app/shared/components/create/create.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Store } from '@ngrx/store';
+import { AppState } from '../../store/app.state';
+import { Tutorial } from '../../store/models/tutorial.model';
+import * as TutorialActions from '../../store/actions/tutorial.action';
+
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
-  styleUrls: ['./create.component.scss']
+  styleUrls: ['./create.component.scss'],
 })
 export class CreateComponent implements OnInit {
+  constructor(private store: Store<AppState>) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  addTutorial(name: string, url: string) {
+    this.store.dispatch(new TutorialActions.AddTutorial({ name, url }));
   }
 
+  ngOnInit(): void {}
 }
