6421032afb63ce8de4ee1dcf14c5ba36e5a10756
heldinho <hfpsis25@gmail.com>
2023-11-03 20:41:07 -0300

    fix
    Signed-off-by: heldinho <hfpsis25@gmail.com>

--- src/app/shared/store/reducers/index.ts
+++ src/app/shared/store/reducers/index.ts
@@ -1,13 +1,17 @@
 import { ActionReducerMap } from '@ngrx/store';
 import { CartModel } from '../models/cart.model';
 import { cartReducer } from './cart.reducer';
+import { Tutorial } from '../models/tutorial.model';
+import { tutorialReducer } from './tutorial.reducer';
 
 export const rootReducer = {};
 
 export interface AppState {
   cart: CartModel;
+  tutorial: Tutorial[];
 }
 
 export const reducers: ActionReducerMap<AppState, any> = {
   cart: cartReducer,
+  tutorial: tutorialReducer,
 };
