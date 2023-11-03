80c730eb81c401ce0828e3cba9a1eac800287a02
heldinho <hfpsis25@gmail.com>
2023-11-03 13:15:30 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonComponent } from './shared/components/button/button.component';
+import { StoreModule } from '@ngrx/store';
+// import { cartReducer } from './shared/store/reducers/cart.reducer';
+import { tutorialReducer } from './shared/store/reducers/tutorial.reducer';
+import { ReadComponent } from './shared/components/read/read.component';
+import { CreateComponent } from './shared/components/create/create.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, ButtonComponent, ReadComponent, CreateComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({
+      // cart: cartReducer,
+      tutorial: tutorialReducer,
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/shared/components/read/read.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { Tutorial } from '../../store/models/tutorial.model';
+import { AppState } from '../../store/app.state';
+
+@Component({
+  selector: 'app-read',
+  templateUrl: './read.component.html',
+  styleUrls: ['./read.component.scss'],
+})
+export class ReadComponent implements OnInit {
+  tutorials: Observable<Tutorial[]>;
+
+  constructor(private store: Store<AppState>) {
+    this.tutorials = store.select('tutorial');
+  }
+
+  ngOnInit(): void {}
+}
